ebc8cd9c7e0ec7880a0039bba56750023352fa4e
Art <pytagpro@gmail.com>
2022-08-26 14:12:33 +0500

    feat: show count color card in stage.

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ import ancientsData from "./data/ancients.js";
 
 const chooseAncients = document.querySelector(".choose-ancients"),
     difficultyBtns = document.querySelectorAll(".difficulty-selection-btns"),
-    stages = document.querySelector(".stages");
+    stages = document.querySelector(".stages"),
+    stage1 = document.querySelector(".stage-1").querySelectorAll("div"),
+    stage2 = document.querySelector(".stage-2").querySelectorAll("div"),
+    stage3 = document.querySelector(".stage-3").querySelectorAll("div");
 
 function showAncients() {
     console.log(ancientsData[0]);
@@ -10,6 +13,16 @@ function showAncients() {
     chooseAncients.appendChild(img);
     img.src = ancientsData[0].cardFace;
     img.addEventListener("click", () => {});
+    console.log(stage1);
+    stage1[0].textContent = ancientsData[0]["firstStage"].greenCards;
+    stage1[1].textContent = ancientsData[0]["firstStage"].blueCards;
+    stage1[2].textContent = ancientsData[0]["firstStage"].brownCards;
+    stage2[0].textContent = ancientsData[0]["secondStage"].greenCards;
+    stage2[1].textContent = ancientsData[0]["secondStage"].blueCards;
+    stage2[2].textContent = ancientsData[0]["secondStage"].brownCards;
+    stage3[0].textContent = ancientsData[0]["thirdStage"].greenCards;
+    stage3[1].textContent = ancientsData[0]["thirdStage"].blueCards;
+    stage3[2].textContent = ancientsData[0]["thirdStage"].brownCards;
 }
 
 difficultyBtns.forEach((btn) => {

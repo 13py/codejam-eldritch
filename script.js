2766308149c8762ce407497d11d584d0abf4e46a
Art <pytagpro@gmail.com>
2022-08-26 11:25:20 +0500

    refactor: change order color stage.

--- script.js
+++ script.js
@@ -9,9 +9,7 @@ function showAncients() {
     let img = document.createElement("img");
     chooseAncients.appendChild(img);
     img.src = ancientsData[0].cardFace;
-    img.addEventListener("click", () => {
-        alert("Привет Даня");
-    });
+    img.addEventListener("click", () => {});
 }
 
 difficultyBtns.forEach((btn) => {
@@ -23,3 +21,5 @@ difficultyBtns.forEach((btn) => {
 });
 
 showAncients();
+
+// Math.floor(Math.random() * (20 - 1 + 1) + 1);

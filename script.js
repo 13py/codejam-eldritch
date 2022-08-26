26d576de10e8c2d2560d410e4b51a3d256d8e9eb
Art <pytagpro@gmail.com>
2022-08-26 09:47:48 +0500

    feat: add to choose level difficulty btns.

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 import ancientsData from "./data/ancients.js";
 
-const chooseAncients = document.querySelector(".choose-ancients");
+const chooseAncients = document.querySelector(".choose-ancients"),
+    difficultyBtns = document.querySelectorAll(".difficulty-selection-btns");
 
 function showAncients() {
     console.log(ancientsData[0]);
     let img = document.createElement("img");
     chooseAncients.appendChild(img);
     img.src = ancientsData[0].cardFace;
+    img.addEventListener("click", () => {
+        alert("Привет Даня");
+    });
 }
+
+difficultyBtns.forEach((btn) => {
+    console.log(btn);
+    btn.addEventListener("click", (e) => {
+        console.log(e.target.textContent);
+    });
+});
 
 showAncients();
